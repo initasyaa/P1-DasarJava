e681b3cabc08c4c80035ec0573bb3683261f59ef
muhamadRamadhaniZ <danii251018@gmail.com>
2022-03-14 15:52:37 +0700

    Variabel Global dan Lokal

---
+++ src/D_BlokdanKomentarr.java
@@ -0,0 +1,17 @@
+public class D_BlokdanKomentarr {
+    static int varGlobal = 34;
+
+    public static void main(String[] args) {
+        varGlobal = 25;
+        System.out.println("varGlobal diakses di method main dengan nilai = "+ varGlobal);
+        double varLokal = 3.14;
+        System.out.println("varLokal hanya bisa di akses di method main dengan nilai = "+varLokal);
+        aksesVariabelLokal();
+    }
+
+    public static void aksesVariabelLokal(){
+        System.out.println("varGlobal bisa diakses di method aksesVariabelLokal dengan nilai = "+varGlobal);
+        //varLokal = 2.5;
+        System.out.println("varLokal tidak bisa diakses di method aksesVariabelLokal ini");
+    }
+}
