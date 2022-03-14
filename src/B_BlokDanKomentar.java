e161998dded4392bc4ece736fb53c1957abab8e9
muhamadRamadhaniZ <danii251018@gmail.com>
2022-03-14 15:15:50 +0700

    Percobaan white space di java

---
+++ src/B_BlokDanKomentar.java
@@ -0,0 +1,4 @@
+/**
+ * sontoh penggunaan white space
+ */
+public class B_BlokDanKomentar { public static void main(String[] args) {System.out.println("Pemrograman berorientasi Obejek dengan Java");}}
