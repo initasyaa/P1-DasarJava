44e1f8dc2898a2452d3d3f64684953e14022e99f
muhamadRamadhaniZ <danii251018@gmail.com>
2022-03-14 16:29:01 +0700

    Mencetak Nilai Konstanta

---
+++ src/G_BlokDanKomentar.java
@@ -0,0 +1,8 @@
+public class G_BlokDanKomentar {
+    public static void main(String[] args) {
+        final double PHI = 3.14;
+        final double CM_PER_INCHI = 2.54;
+        System.out.println("Nilai Konstanta PHI = "+ PHI);
+        System.out.println("Nilai Konstanta CM_PER_INCHI = "+CM_PER_INCHI);
+    }
+}
