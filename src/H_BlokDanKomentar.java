568130cd950db97f876819b58ccab8cc2a205f1a
muhamadRamadhaniZ <danii251018@gmail.com>
2022-03-14 16:32:45 +0700

    Mencetak Literal Number

---
+++ src/H_BlokDanKomentar.java
@@ -0,0 +1,12 @@
+public class H_BlokDanKomentar {
+    public static void main(String[] args) {
+        int distance = 25;
+        final long MM_TO_KM = 1000000L;
+        float latitude = 109.234567f;
+        final double PHI = 3.14;
+        System.out.println("Nilai Literal int = "+distance);
+        System.out.println("Nilai Literal long = "+MM_TO_KM);
+        System.out.println("Nilai Literal float = "+latitude);
+        System.out.println("Nilai Literal double = "+PHI);
+    }
+}
