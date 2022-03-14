4fb39b7d6a81b212ce7a3957932e413ada31c645
muhamadRamadhaniZ <danii251018@gmail.com>
2022-03-14 14:52:58 +0700

    initial projek pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,14 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemograman Berorientasi dengan Java");
+        //contoh komentar 1 baris
+
+        /*contoh komentar lebih dari satu baris
+        lalalalala
+        */
+
+        /**
+         * Java Doc --> baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
